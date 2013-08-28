50847d0f7261bc9346d389f995e8fee639d5678c
Nathan Rajlich <nathan@tootallnate.net>
2013-08-27 22:37:23 -0700

    add link to einaros/ws issue about options arg

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ var WebSocket = global.WebSocket || global.MozWebSocket;
 module.exports = WebSocket ? ws : null;
 
 /**
- * WebSocket constructor. The third `opts` options object gets ignored in web
- * browsers, since it's non-standard, and throws a TypeError if passed to the
- * constructor.
+ * WebSocket constructor.
+ *
+ * The third `opts` options object gets ignored in web browsers, since it's
+ * non-standard, and throws a TypeError if passed to the constructor.
+ * See: https://github.com/einaros/ws/issues/227
  *
  * @param {String} uri
  * @param {Array} protocols (optional)
